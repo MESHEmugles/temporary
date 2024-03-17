2d5439ad86863dc9bdbb5e71f7cc68d1dd7b2116
phlenfyl <mdpeter28@gmail.com>
2024-03-18 00:22:54 +0100

    heading correction

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ import Team from "@/components/about/TheTeam";
 
 export default function About() {
   return (
-    <div className="sm:bg-[#d3d9d9] mx-0 md:bg-[#EBEBEB] pb-20 shadow-none xl:mx-[8em] xl:shadow-lg 2xl:mx-[35em] xl:shadow-xl">
+    <div className="sm:bg-[#d3d9d9] mx-0 md:bg-[#EBEBEB] pb-20 shadow-none xl:mx-[8em] xl:shadow-lg 2xl:mx-[35em] 2xl:shadow-xl">
       <Aboutanime delay={0.3}>
         <Banner />
 
